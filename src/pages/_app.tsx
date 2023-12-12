a003eda0b861ef2ba3de85b74d14d7ec76279282
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-12-12 20:25:58 +0700

    setup prettier and chakraUI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/randy-travel/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+
+import "@/randy-travel/styles/globals.css";
+import theme from "../theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
