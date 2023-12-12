102f5fa4e8df449e350f1d921fcd904ce0c2a2c2
rasyidrmhd <rasyidrmuhammad@gmail.com>
2023-12-12 20:52:05 +0700

    added head and set font to inter

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,31 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import { Inter } from "next/font/google";
 import type { AppProps } from "next/app";
 
 import "@/randy-travel/styles/globals.css";
 import theme from "../theme";
+import Head from "next/head";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <title>Randy Travel Solution</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Randy Travel Solution" />
+        <meta property="og:title" content="Randy Travel Solution" />
+        <meta property="og:description" content="Randy Travel Solution" />
+        <meta property="og:image" content="/logo-text.svg" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+      <ChakraProvider theme={theme}>
+        <main className={`${inter.className}`}>
+          <Component {...pageProps} />
+        </main>
+      </ChakraProvider>
+    </>
   );
 }
